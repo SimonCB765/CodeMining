88c409c733ba0b1c14ccad97e62ecd1717fb5ce1
unknown <SimonCB765@gmail.com>
2015-12-21 16:11:59 +0000

    Added basic axes

--- AmbiguousRecords.js
+++ AmbiguousRecords.js
@@ -26,15 +26,41 @@ d3.tsv("/Data/DisambiguationResults.tsv", dataAccessorFunction, function(error, 
         // Create the figure the type 1 vs type 2 diabetes results.
         var figure1v2 = svg.append("g")
             .attr("transform", "translate(0, 0)");
-        createFigure(figure1v2, data.map(function(d) { return d.First; }));
+        createFigure(figure1v2, data.map(function(d, index) { return {datum : d.First, index : index}; }));
 
         // Create the figure for the diabetes vs non-diabetes results.
         var figureDvsND = svg.append("g")
             .attr("transform", "translate(" + (svgWidth / 2) + ", 0)");
-        createFigure(figureDvsND, data.map(function(d) { return d.Second; }));
+        createFigure(figureDvsND, data.map(function(d, index) { return {datum : d.Second, index : index}; }));
     }
 );
 
 function createFigure(figureContainer, dataArray)
 {
+    // Define positioning variables.
+    var figurePadding = {top: 30, right: 30, bottom: 30, left: 30};  // Padding around the graph in the figure to allow for titles and labelling axes.
+
+    // Create scales for the figure.
+    var xScale = d3.scale.linear()
+        .domain([0, dataArray.length])
+        .range([0, figureWidth - figurePadding.left - figurePadding.right]);
+    var yScale = d3.scale.linear()
+        .domain([0.0, 1.0])
+        .range([figureHeight - figurePadding.top - figurePadding.bottom, 0]);
+
+    // Add the axes for the figure.
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient("bottom");
+    xAxis = figureContainer.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + figurePadding.left + ", " + (figureHeight - figurePadding.bottom) + ")")
+        .call(xAxis);
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient("left");
+    yAxis = figureContainer.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + figurePadding.left + ", " + figurePadding.bottom + ")")
+        .call(yAxis);
 }
