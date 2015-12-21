cd2bc875f1dccdadcef2dcf3a2a6ae366ea66573
unknown <SimonCB765@gmail.com>
2015-12-21 12:15:31 +0000

    Initial file setup

---
+++ AmbiguousRecords.js
@@ -0,0 +1,8 @@
+svgWidth = 600;
+svgHeight = 600;
+
+// Create the SVG element.
+var svg = d3.select("body")
+    .append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
