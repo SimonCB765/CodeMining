b7d3d08546dfb63485948f87984f396d21f409ff
unknown <SimonCB765@gmail.com>
2016-01-04 12:37:17 +0000

    Added basic model drawing

--- Workflow.js
+++ Workflow.js
@@ -26,10 +26,11 @@ var hGapBetweenPictures = 100;
 var yOffset = 200;
 var xOffset = 10;
 
-// Define dataset size variables.
+// Define dataset and model size variables.
 var rawDatasetDimensions = { height : 150, width : 100 };
 var processedDatasetDimensions = { height : rawDatasetDimensions.height * 2 / 3, width : rawDatasetDimensions.width };
 var unusedDatasetDimensions = { height : rawDatasetDimensions.height / 3, width : rawDatasetDimensions.width };
+var initialModelDimensions = processedDatasetDimensions
 
 // Create the raw data picture.
 var rawDataPosition = { x : xOffset, y : yOffset };
@@ -59,6 +60,14 @@ var arrowStartY = rawDataPosition.y + (rawDatasetDimensions.height / 2);
 createArrow(svg, arrowStartX, arrowStartY, processedDataPosition.x, processedDataPosition.y + (processedDatasetDimensions.height / 2));
 createArrow(svg, arrowStartX, arrowStartY, unusedDataPosition.x, unusedDataPosition.y + (unusedDatasetDimensions.height / 2));
 
+// Create the initial model.
+xOffset += processedDatasetDimensions.width + hGapBetweenPictures;
+var initialModelPosition = { x : xOffset, y : processedDataPosition.y };
+var containerInitialModel = svg.append("g")
+    .classed("initialModel", true)
+    .attr("transform", "translate(" + initialModelPosition.x + ", " + initialModelPosition.y + ")");
+createModel(containerInitialModel, 0, 0, initialModelDimensions.width, initialModelDimensions.height, "Initial Model");
+
 function createArrow(selection, startX, startY, endX, endY)
 {
     var arrow = selection.append("path")
@@ -69,6 +78,51 @@ function createArrow(selection, startX, startY, endX, endY)
     arrow
         .attr("d", "M" + startX + "," + startY + "C" + contrl1.x + "," + contrl1.y + "," + contrl2.x + "," + contrl2.y + "," + endX + "," + endY)
         .attr("marker-end", "url(#arrowhead)");
+}
+
+function createModel(selection, x, y, width, height, text)
+{
+    selection.append("rect")
+        .attr("class", "modelEdge")
+        .attr("x", x)
+        .attr("y", y)
+        .attr("width", width)
+        .attr("height", height);
+
+    selection.append("rect")
+        .attr("class", "modelInternal")
+        .attr("x", x)
+        .attr("y", y)
+        .attr("width", width)
+        .attr("height", height);
+
+    // Add text.
+    var textToAdd = selection.append("text")
+        .attr("text-anchor", "middle")
+        .attr("x", width / 2)
+        .text(text);
+    var textBBox = textToAdd.node().getBBox();
+    textToAdd.attr("y", y - (textBBox.height * 1 / 2));
+
+    // Add data points.
+    var data = [ { x : 10, y : 10 }, { x : 20, y : 20 }, { x : 30, y : 30 }, { x : 40, y : 40 }, { x : 50, y : 50 }, { x : 60, y : 60 }, { x : 70, y : 70 } ];
+    var datapoints = selection.selectAll("circle")
+        .data(data)
+            .enter()
+            .append("circle")
+                .classed("modelData", true)
+                .attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; })
+                .attr("r", 3);
+
+    // Add the line.
+    var start = { x : x + (width / 10), y : y + (height * 9 / 10) };
+    var end = { x : x + (width * 9 / 10), y : y + (height / 10) };
+    var control1 = { x : 40, y : 40 };
+    var control2 = { x : 50, y : 50 };
+    var fitLine = selection.append("path")
+        .classed("modeLine", true)
+        .attr("d", "M" + start.x + "," + start.y + "C" + control1.x + "," + control2.y + "," + control2.x + "," + control2.y + "," + end.x + "," + end.y);
 }
 
 function createTable(selection, x, y, width, height, text)
