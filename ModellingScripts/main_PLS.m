42f7abb82ad5cfa3e7140711c8a2affae218dbd5
unknown <SimonCB765@gmail.com>
2016-02-16 16:38:57 +0000

    First commit for the PLS approach

---
+++ ModellingScripts/main_PLS.m
@@ -0,0 +1,56 @@
+function main_PLS(inputData, codeMapping, positiveCodes, positiveChildren, negativeCodes, negativeChildren, foldsToUse, discardThreshold, outputDir)
+    % Perform clinical code identification using partial least squares regression.
+    %
+    % Positive and negative examples for the model training will be determined based on the supplied codes.
+    % This is achieved by taking any example that contains a positive code to be a positive example, and any example containing
+    %     a negative code to be a negative example. Any examples that contain both positive and negative codes will not be used for
+    %     training, but will have a separate disambiguation prediction performed at the end to see if their class can be determined
+    %     using the final model.
+    % The supplied codes can also be augmented with their child terms (through positiveChildren and negativeChildren).
+    %     For example, if positiveCodes is C10E, then setting positiveChildren to true will result in C10E and all its child terms
+    %     being used to determine positive examples.
+    %
+    % Keyword Arguments
+    % inputData - The file containing the patient record dataset. Each line of the file should be formatted as:
+    %                 PatentID\tCode\tOccurrences
+    %             Occurrences records the number of times code Code appears in the record of the patient with ID PatientID.
+    %             Both PatientID and Code are likely to appear on more than one line (as patients normally have more than one code associated them
+    %             and codes are normally associated with more than one patient).
+    % codeMapping - File containing a mapping between codes and their descriptions. Each line of the file should be formatted as:
+    %                   Code\tDescription
+    % positiveCodes - A list of codes that will be used to determine positive examples. The codes should be supplied as a comma
+    %                 separated list, e.g. C10E,C10F. If the empty string is supplied, then it is taken to mean that all examples not
+    %                 determined to be negative will be positive. Only one of positiveCodes and negativeCodes can be empty.
+    % positiveChildren - True or false depending on whether child terms of the positiveCodes should be used in determining positive examples.
+    % negativeCodes - A list of codes that will be used to determine negative examples. The codes should be supplied as a comma
+    %                 separated list, e.g. C10E,C10F. If the empty string is supplied, then it is taken to mean that all examples not
+    %                 determined to be positive will be negative. Only one of positiveCodes and negativeCodes can be empty.
+    % negativeChildren - True or false depending on whether child terms of the negativeCodes should be used in determining negative examples.
+    % foldsToUse - The number of folds to use for cross validation.
+    % discardThreshold - The threshold at which examples from the training set will be discarded after training the initial model.
+    %                    Positive examples will be discarded if their posterior probability is < discardThreshold, while negative examples will
+    %                    be discarded if their posterior probability is > discardThreshold. This is symmetric as the posterior here indicates the probability
+    %                    of the class of the example being positive. Therefore, keeping negative examples with posterior < discardThreshold is the same
+    %                    as keeping all negative examples with a > discardThreshold probability of being negative.
+    % outputDir - The directory to save the results in (should be supplied without a trailing /). If outputDir is the empty string, then
+    %             the results are saved in the directory the script is called from.
+
+    % Check arguments are valid.
+    % 1) check that the codes aren't both empty yadda yadda
+    % 2) discardThreshold is between 0-1
+    % 3) CV folds is an integer
+    % 4) outputDir is set to '/' if called as the empty string.
+
+    % Setup the results directory.
+    if (exist(outputDir, 'dir') == 7)
+        % If the output directory already exists, then delete it before creating it.
+        rmdir(outputDir, 's');
+    end
+    mkdir(outputDir);
+
+    % Load the patient data and stick it in a sparse matrix.
+    fidDataset = fopen(inputData, 'r');
+    data = textscan(fidDataset, '%d %s %d');
+    fclose(fidDataset);
+
+end
