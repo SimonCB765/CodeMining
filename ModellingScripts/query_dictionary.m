20da94db301dd5a9380104fc70e62f88ac4152f6
SimonCB765 <SimonCB765@gmail.com>
2016-02-24 09:57:13 +0000

    Added comments

--- ModellingScripts/query_dictionary.m
+++ ModellingScripts/query_dictionary.m
@@ -1,7 +1,18 @@
 function [codeDescription] = query_dictionary(codeMap, code)
+    % Safely access the mapping from codes to descriptions.
+    % The code makes the assumption that all valid clinical codes have a description in the mapping, and therefore that
+    % any missing codes are for vendor specific drugs.
+    %
+    % Keyword Arguments
+    % codeMap - containers.Map mapping each code to its description.
+    % code - The code to extract the description for.
 
-if isKey(codeMap, code)
-    codeDescription = codeMap(code);
-else
-    codeDescription = 'Unknown vendor specific drug';
+    if isKey(codeMap, code)
+        % If the code is in the map, then get its description.
+        codeDescription = codeMap(code);
+    else
+        % If the code is not in the map, then it is an unknown vendor specific drug.
+        codeDescription = 'Unknown vendor specific drug';
+    end
+
 end
