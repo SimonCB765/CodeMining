14cffa1bad6bc3f33ecb605875b88477c7ea95dd
sb0080 <s.c.bull@surrey.ac.uk>
2016-03-17 10:53:24 +0000

    Added correction to handle unicode in JSON

--- Code/CodeMining/main.py
+++ Code/CodeMining/main.py
@@ -60,6 +60,7 @@ def main(args):
             readParserArgs = open(fileParserArgs, 'r')
             parserArgs = json.load(readParserArgs)
             readParserArgs.close()
+            parserArgs = json_to_ascii(parserArgs)  # Convert all unicode characters to ascii (only needed for Python < 3).
 
             # Check that the delimiter is a string.
             if (("Delimiter" in parserArgs) and (not is_string(parserArgs["Delimiter"]))):
@@ -123,6 +124,7 @@ def main(args):
         readParserArgs = open(fileParserArgs, 'r')
         parserArgs = json.load(readParserArgs)
         readParserArgs.close()
+        parserArgs = json_to_ascii(parserArgs)  # Convert all unicode characters to ascii (only needed for Python < 3).
 
         # Process the arguments.
         delimiter = '\t' if ("Delimiter" not in parserArgs) else parserArgs["Delimiter"]
@@ -162,6 +164,28 @@ def is_string(objectToCheck):
 
     majorVersion = sys.version_info[0]  # Determine major version number.
     return isinstance(objectToCheck, str) if (majorVersion == 3) else isinstance(objectToCheck, basestring)
+
+
+def json_to_ascii(jsonObject):
+    """Convert a JSON object from unicode to ascii strings.
+
+    This will recurse through all levels of the JSON dictionary, and therefore may hit Python's recursion limit.
+    To avoid this use object_hook in the json.load() function instead.
+
+    """
+
+    if isinstance(jsonObject, dict):
+        # If the current part of the JSON oobject is a dictionary, then make all its keys and values ascii if needed.
+        return dict([(json_to_ascii(key), json_to_ascii(value)) for key, value in jsonObject.iteritems()])
+    elif isinstance(jsonObject, list):
+        # If the current part of the JSON object is a list, then make all its elements ascii if needed.
+        return [json_to_ascii(i) for i in jsonObject]
+    elif isinstance(jsonObject, unicode):
+        # If you've reached a unicode string convert it to ascii.
+        return jsonObject.encode('utf-8')
+    else:
+        # You've reached a non-unicode terminus (e.g. an integer or null).
+        return jsonObject
 
 
 if __name__ == '__main__':
