9c59a2b6b5047c1efcc0d62297bfa2a27f7f3199
unknown <SimonCB765@gmail.com>
2016-04-06 14:27:35 +0100

    Added status outputs

--- Code/CodeMining/main_mini_batch.m
+++ Code/CodeMining/main_mini_batch.m
@@ -339,6 +339,11 @@ function main_mini_batch(fileParams)
             for bSize = 1:numel(batchSizeValues)
                 for aVal = 1:numel(alphaValues)
                     for lVal = 1:numel(lambdaValues)
+                        % Display current test.
+                        disp(strcat({'Current time - '}, datestr(now)));
+                        disp(strcat({'\tNow running - '}, sprintf('Iterations=%d\tBatch Size=%d\tAlpha=%1.4f\tLambda=%1.4f', ...
+                            maxIterValues(numIter), batchSizeValues(bSize), alphaValues(aVal), lambdaValues(lVal))))
+
                         % Create, train and evaluate the first model on the training set.
                         regLogModelFirst = RegMultinomialLogistic(alphaValues(aVal), batchSizeValues(bSize), lambdaValues(lVal), maxIterValues(numIter));
                         recordOfDescentFirst = regLogModelFirst.train(trainingMatrix, trainingTarget, []);
@@ -490,6 +495,11 @@ function main_mini_batch(fileParams)
             for bSize = 1:numel(batchSizeValues)
                 for aVal = 1:numel(alphaValues)
                     for lVal = 1:numel(lambdaValues)
+                        % Display current test.
+                        disp(strcat({'Current time - '}, datestr(now)));
+                        disp(strcat({'\tNow running - '}, sprintf('Iterations=%d\tBatch Size=%d\tAlpha=%1.4f\tLambda=%1.4f', ...
+                            maxIterValues(numIter), batchSizeValues(bSize), alphaValues(aVal), lambdaValues(lVal))))
+
                         % Generate the record of the class prediction for each example in the full training set.
                         predictions = zeros(numel(trainingTarget), numberOfClasses);
                         maxProbClass = zeros(numel(trainingTarget), 1);
