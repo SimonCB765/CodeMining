d8ae54c2fccf85df4a95a5573e95a3bc93ee4eda
unknown <SimonCB765@gmail.com>
2016-04-09 13:38:08 +0100

    Added determining of examples in each class

--- Code/CodeMiningPython/parse_classes.py
+++ Code/CodeMiningPython/parse_classes.py
@@ -1,16 +1,65 @@
-"""Utility functions for parsing and checking the class definitions in the user's parameter file."""
+"""Functions to check the class definitions in the user's parameter file and find patients in each class."""
 
 # Python imports.
+import itertools
+
+# 3rd party imports.
+import numpy as np
 
 # User imports.
+import extract_child_codes
 
 
-def parse_classes():
+def find_patients(dataMatrix, classData, mapCodeToIndex):
+    """Determine the indices of the patients in each class.
+
+    Patients are deemed to be in a class if they have a nonzero value for a code belonging to the class.
+    E.g. if class A consists of codes C10E and C10F, then any example with a nonzero value for one
+    of these codes will belong to class A.
+
+    Ambiguous examples are those belonging to more than one class.
+
+    :param dataMatrix:      Matrix containing the dataset.
+    :type dataMatrix:       scipy sparse matrix
+    :param classData:       Information about how to determine the classes.
+    :type classData:        dict
+    :param mapCodeToIndex:  A mapping from codes to their indices in the dataset.
+    :type mapCodeToIndex:   dict
+    :return :               The lists of which examples belong to which class, and which examples are ambiguous.
+    :rtype :                dict
+
     """
 
-    :return:
+    # For each class determine the indices of the codes that define the class, and the patients in the class.
+    # Any codes in the parameter list that do not appear in the dataset will be ignored.
+    classExamples = {}
+    for i in classData:
+        # Get variable indices.
+        classCodeIndices = [j for j in classData[i] if j[-1] != '.']
+        getChildren = [j[:-1] for j in classData[i] if j[-1] == '.']  # Need to get children for any code ending in '.'.
+        getChildren = extract_child_codes.main(getChildren, mapCodeToIndex.keys())
+        classCodeIndices.extend(getChildren)
+        classCodeIndices = [mapCodeToIndex.get(j, None) for j in classCodeIndices]
+        classCodeIndices = [j for j in classCodeIndices if j]  # Remove all None values from the list of indices.
 
-    """
+        # Determine patients. For a dense matrix np.where(dataMatrix > 0) could be used, but np.where
+        # doesn't work for sparse matrices.
+        classSubset = dataMatrix[:, classCodeIndices] > 0  # Subset of the dataset containing the class's examples.
+        exampleIndices = set(np.nonzero(classSubset)[0])  # The indices of the patients in the class.
+        classExamples[i] = exampleIndices
+
+    # Determine the ambiguous examples.
+    ambiguousExamples = set([])
+    for i, j in itertools.combinations(classExamples.keys(), 2):
+        ambiguousExamples |= classExamples[i] & classExamples[j]
+
+    # Remove the ambiguous examples from all classes.
+    for i in classExamples:
+        classExamples[i] -= ambiguousExamples
+        classExamples[i] = sorted(classExamples[i])
+    classExamples["Ambiguous"] = sorted(ambiguousExamples)
+
+    return classExamples
 
 
 def check_validity(classDict):
