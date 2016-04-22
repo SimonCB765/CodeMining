dc3c1e64f819bbb2cc117b7a5936752f2531836c
unknown <SimonCB765@gmail.com>
2016-04-22 13:44:16 +0100

    Altered to check token type is correct

--- Code/TestPython/test_lexer.py
+++ Code/TestPython/test_lexer.py
@@ -5,6 +5,7 @@ import unittest
 
 # User imports.
 from CodeMiningPython import ClassLexer
+from CodeMiningPython import tokens
 
 
 class SuccessTests(unittest.TestCase):
@@ -18,14 +19,16 @@ class SuccessTests(unittest.TestCase):
             "#3 > #7",
             "D. < #5 ~ #6 > ~ FF"]
         results = [
-            [("CODE", "CODE1"), ('EOF', 'EOF')],
-            [("CODE", "CODE1."), ('EOF', 'EOF')],
-            [("CODE", "C1"), ("AND_OP", "&"), ("CODE", "c2"), ("OR_OP", "|"), ("CODE", "c3"), ('EOF', 'EOF')],
-            [("NUM", '3'), ("COMP_OP", ">"), ("NUM", '7'), ('EOF', 'EOF')],
-            [("CODE", "D."), ("COMP_OP", "<"), ("NUM", '5'), ("NOT_OP", "~"), ("NUM", '6'), ("COMP_OP", ">"),
-                ("NOT_OP", "~"), ("CODE", "FF"), ('EOF', 'EOF')]]
+            [tokens.CodeLiteralToken("CODE1"), tokens.EOFToken()],
+            [tokens.CodeLiteralToken("CODE1."), tokens.EOFToken()],
+            [tokens.CodeLiteralToken("C1"), tokens.AndToken(), tokens.CodeLiteralToken("c2"), tokens.OrToken(),
+                tokens.CodeLiteralToken("c3"), tokens.EOFToken()],
+            [tokens.NumLiteralToken("7"), tokens.GreaterThanToken(), tokens.NumLiteralToken("7"), tokens.EOFToken()],
+            [tokens.CodeLiteralToken("D"), tokens.LessThanToken(), tokens.NumLiteralToken("5"), tokens.NotToken(),
+                tokens.NumLiteralToken("6"), tokens.GreaterThanToken(), tokens.NotToken(),
+                tokens.CodeLiteralToken("FF"), tokens.EOFToken()]]
 
         for i in zip(tests, results):
             lexer = ClassLexer.ClassLexer(i[0])  # Create the lexer.
             lexer.tokenise()  # Tokenise the input.
-            self.assertEqual(lexer.tokenised, i[1])
+            self.assertEqual([type(j) for j in lexer.tokenised], [type(j) for j in i[1]])
